e231b3f37cabf4363802b993dc6fd08bd387a162
Burtsev Nikita <bur.19091@gmail.com>
2024-02-21 02:28:49 +0300

    Industrial Engineering lab3 tasks 1 and 2 (3 incoming)

---
+++ src/main/java/PPEVM/lab3/Book.java
@@ -0,0 +1,111 @@
+package PPEVM.lab3;
+
+public class Book {
+    private int id;
+    private String name;
+    private String authors;
+    private String publishing;
+    private int yearOfPublishing;
+    private int pages;
+    private int price;
+    private String binding;
+
+    public Book(int id, String name, String authors, String publishing, int yearOfPublishing, int pages, int price, String binding) {
+        this.id = id;
+        this.name = name;
+        this.authors = authors;
+        this.publishing = publishing;
+        this.yearOfPublishing = yearOfPublishing;
+        this.pages = pages;
+        this.price = price;
+        this.binding = binding;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public void setPublishing(String publishing) {
+        this.publishing = publishing;
+    }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setBinding(String binding) {
+        this.binding = binding;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public String getPublishing() {
+        return publishing;
+    }
+
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getBinding() {
+        return binding;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                ", Название: '" + name + '\'' +
+                ", Автор(ы): '" + authors + '\'' +
+                ", Издательство: '" + publishing + '\'' +
+                ", Год издания: " + yearOfPublishing +
+                ", Количество страниц: " + pages +
+                ", Переплет: '" + binding + '\'' +
+                ", Цена: " + price;
+    }
+
+    public boolean checkAuthor(String author) {
+        return this.authors.contains(author);
+    }
+
+    public boolean checkPublishing(String publishing) {
+        return this.publishing.equals(publishing);
+    }
+
+    public boolean checkYear(int year) {
+        return this.yearOfPublishing >= year;
+    }
+}
